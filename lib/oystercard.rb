fa12d65c3fcf85d0c1190b89b242b7ef56e89c3a
Mio Chung <miochung7@gmail.com>
2020-12-15 16:23:28 +0000

    completed Q10

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,35 @@
 class Oystercard
   LIMIT = 70
+  MINIMUM = 1
+  CHARGE = 5
+
   attr_reader :balance
+  attr_accessor :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(number)
     fail "Maximum limit of #{LIMIT} exceeded" if number + @balance > LIMIT
     @balance += number
   end
+
+  def touch_in
+    fail 'Please top up!' if @balance < MINIMUM
+    @in_journey = true
+  end
+
+  def touch_out
+    deduct(CHARGE)
+    @in_journey = false
+    
+  end
+
+  private
+  def deduct(number)
+    @balance -= number
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,19 +1,18 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:oystercard) { Oystercard.new}
+
   describe 'initialisation - balance' do
     it 'shows a default balance of 0' do
-      oystercard = Oystercard.new
       expect(oystercard.balance).to eq 0
     end
   end
 
   describe '#topup' do
-
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'adds money onto the card' do
-      oystercard = Oystercard.new
       # oystercard.add(5)
       # expect(oystercard.balance).to eq 5
       expect{ oystercard.top_up 5 }.to change{ oystercard.balance }.by 5
@@ -22,10 +21,56 @@ describe Oystercard do
     it 'raises an error if balance is more than £90' do
       # oystercard = Oystercard.new
       # expect{ oystercard.top_up(95) }.to raise_error('Maximum limit is £90')
-      oystercard = Oystercard.new
       limit = Oystercard::LIMIT
       oystercard.top_up(limit)
       expect { oystercard.top_up 1}.to raise_error("Maximum limit of #{limit} exceeded")
+    end
+  end
+
+  # describe '#deduct' do
+  #   it 'deducts money from the balance' do
+  #     oystercard.top_up(20)
+  #     oystercard.deduct(10)
+  #     expect(oystercard.balance).to eq (10)
+  #     # expect{ oystercard.deduct 10}.to change{ oystercard.balance }.by -10
+  #   end
+  # end
+
+  describe '#in_journey?' do
+    it 'tracks the status of the journey' do
+      expect(oystercard.in_journey).to eq false
+      # expect(subject).not_to be_in_journey
+    end
+  end
+
+  describe '#touch_in' do
+    it { is_expected.to respond_to :touch_in }
+
+    it 'changes the status of #in_journey to true' do
+      oystercard.top_up(30)
+      oystercard.touch_in
+      expect(oystercard.in_journey).to eq true
+    end
+
+    it 'should raise an error if balance is less than £1' do
+      expect{ oystercard.touch_in }.to raise_error 'Please top up!'
+    end
+  end
+
+  describe '#touch_out' do
+    it { is_expected.to respond_to :touch_out }
+
+    it 'changes the status of #in_journey to false' do
+      # oystercard.top_up(30)
+      oystercard.touch_out
+      expect(oystercard.in_journey).to eq false
+    end
+
+    it 'deducts the correct amount from the balance' do
+      oystercard.top_up(40)
+      oystercard.touch_in
+      # p oystercard.touch_out(10)
+      expect{ oystercard.touch_out }.to change{ oystercard.balance }.by (-Oystercard::CHARGE)
     end
   end
 
